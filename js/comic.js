d16f3158db1874cf02cbedd39291ebf60bf9b7d1
63829 <63829@gymnasium.ax>
2024-12-11 10:48:35 +0200

    lagade css så den är bättre

--- js/comic.js
+++ js/comic.js
@@ -1,25 +1,50 @@
-var maxComic 
-var currentComic
+//Definierar globala variabler för nuvarande och största comic nummer
+var maxComic = -1;
+var currentComic = -1;
 
-window.onload = function() {
-    // hämta senaste comic
+
+window.onload = function(){
+    //hämta senaste comic
     getComic('latest');
-    // sätter funkonalitet för nav knappar
+    //sätter funktionalitet för nav knappar
+    document.getElementById('first').addEventListener('click',function(){
+        if(currentComic!=1){
+            getComic(1);
+        }
+    })
 }
 
-function getComic(which) {
-    //hämta (fetch) data från xkcd api
-    fetch('https://xkcd.vercel.app/?comic='+ which);
+function getComic(which){
+    //Hämta(fetch) data från xkcd api
+    fetch('https://xkcd.vercel.app/?comic='+which)
         .then(function(response){
-            //kolla att ok 200
-            if (response.status === 200) {
+            //Kolla om svaret är ok(200)
+            if(response.status === 200){
                 return response.json();
             } else {
-                // throw error
-                throw new Error;
+                //Kastar ett felmeddelande om status inte är ok
+                throw new Error('Failed to load comic');
             }
         })
         .then(function(data){
-            
+            //Uppdatera maxComic värde
+            if(maxComic < data.num){
+                maxComic=data.num;
+            }
+            //Skicka json data för behandling till DOM
+            console.log(data);
+            appendComic(data);
         })
+        .catch(function(error){
+            //logga eventuella errors
+            console.log('Error: ', error);
+        })
+}
+
+function appendComic(data){
+    let img = document.getElementById(theComic);
+    img.alt = data.title;
+    console.log(data.img);
+    img.src = data.img;
+    document.getElementById("linkTo").innerHTML = data.img;
 }
